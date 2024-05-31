45ed584ff801fac1f8880b863877d71252b45345
tapbi_haipd <hai.phanduc@tapbi.vn>
2024-05-31 17:29:50 +0700

    update

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/hello")
+async def say_hello():
+    return {"message": f"Hello Group"}
